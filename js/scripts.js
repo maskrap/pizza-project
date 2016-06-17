d75afcfc8dbe9648c963dd26a1f4051f4917c2ea
Sam Park <sampark18@gmail.com>
2016-06-17 14:03:54 -0700

    Allow pizza size to be calculated but still needs the toppings to be calculated

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,36 @@
 
 // Backend logic
-function Pizza(size, toppings, price) {
+function Pizza(size, price) {
   this.size = size;
-  this.toppings = toppings;
+  this.toppings = [];
   this.price = price;
-}
-
+};
 
 // this checks the size of the pizza and will help determine what size will be shown on the final order
-Pizza.prototype.calc = function (size) {
-  function sizeCheck () {
-    var smallPizza =  document.getElementbyId("input#small").checked;
-    var mediumPizza =  document.getElementbyId("input#medium").checked;
-    var largePizza =  document.getElementbyId("input#large").checked;
-    var sizeCost = 0;
-
-    if (smallPizza === true)  {
-      document.getElementbyId("input#medium").checked = false;
-      document.getElementbyId("input#large").checked = false;
-      sizeCost += 8;
-    } else if (mediumPizza === true)  {
-      document.getElementbyId("input#small").checked = false;
-      document.getElementbyId("input#large").checked = false;
-      sizeCost += 10;
-    } else {
-      document.getElementbyId("input#small").checked = false;
-      document.getElementbyId("input#medium").checked = false;
-      sizeCost += 12;
-
-      console.log("You either picked a large pizza or you fucked up");
-    }
-  }
+Pizza.prototype.calc = function () {
+  var toppings = this.toppings.length;
+  console.log(toppings);
+  var total = this.size + toppings;
+  return total;
+  console.log("If you see this, you're good!");
 };
 
 
 // Front-end logic
 $(document).ready(function() {
-  var pizzaClient;
 
   $("form#pizza-custom").submit(function(event) {
+    debugger;
     event.preventDefault();
-    var pizzaClient = new Pizza();
+    var sizeChoice = parseInt($("input[type='radio'][name='optionsRadios']:checked").val());
+    var newPizza = new Pizza(sizeChoice);
+    var finalOrder = newPizza.calc();
 
-    // sizeCheck();
-    // var inputtedSize = $(input#pizza-custom).val();
+    $.each($("input[type='checkbox'][name='toppings']:checked"), function() {
+      newPizza.toppings.push($(this).val());
+    });
 
+    $("#finalorder").text(finalOrder);
+    $("#finaltoppings").append("<li>" + finalOrder + "</li>")
   });
 });
