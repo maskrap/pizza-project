2d28d522c722334ef16d5c2290a4f19bbd16a381
Sam Park <sampark18@gmail.com>
2016-06-17 15:24:50 -0700

    I am stupid and changing the jQuery version fixed my issue with the toppings not adding up

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,8 @@ function Pizza(size, price) {
 
 // this checks the size of the pizza and will help determine what size will be shown on the final order
 Pizza.prototype.calc = function () {
-  var toppings = this.toppings.length;
-  console.log(toppings);
-  var total = this.size + toppings;
+  var toppingsAdded = this.toppings.length * 2;
+  var total = this.size + toppingsAdded;
   return total;
   console.log("If you see this, you're good!");
 };
@@ -20,17 +19,15 @@ Pizza.prototype.calc = function () {
 $(document).ready(function() {
 
   $("form#pizza-custom").submit(function(event) {
-    debugger;
     event.preventDefault();
     var sizeChoice = parseInt($("input[type='radio'][name='optionsRadios']:checked").val());
     var newPizza = new Pizza(sizeChoice);
-    var finalOrder = newPizza.calc();
 
-    $.each($("input[type='checkbox'][name='toppings']:checked"), function() {
+    $.each($("input[name='toppings']:checked"), function() {
       newPizza.toppings.push($(this).val());
     });
-
+    var finalOrder = newPizza.calc();
     $("#finalorder").text(finalOrder);
-    $("#finaltoppings").append("<li>" + finalOrder + "</li>")
+    // $("#finaltoppings").append("<li>" + newPizza + "</li>")
   });
 });
