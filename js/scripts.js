9f6db642ce4a37b4976f4e8557cd5165f3ca36df
Sam Park <sampark18@gmail.com>
2016-06-23 11:11:12 -0700

    Removed price property that was not being used

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,23 @@
-// Backend logic
-function Pizza(size, price) {
+function Pizza(size) {
   this.size = size;
   this.toppings = [];
-  this.price = price;
 };
 
-// this checks the size of the pizza and will help determine what size will be shown on the final order
 Pizza.prototype.calc = function () {
   var toppingsAdded = this.toppings.length * 2;
   var total = this.size + toppingsAdded;
   return total;
-  console.log("If you see this, you're good!");
 };
 
-
-// Front-end logic
 $(document).ready(function() {
-
   $("form#pizza-custom").submit(function(event) {
     event.preventDefault();
     var sizeChoice = parseInt($("input[type='radio'][name='optionsRadios']:checked").val());
     var newPizza = new Pizza(sizeChoice);
-
     $.each($("input[name='toppings']:checked"), function() {
       newPizza.toppings.push($(this).val());
     });
     var finalOrder = newPizza.calc();
     $("#finalorder").text(finalOrder);
-    // $("#finaltoppings").append("<li>" + newPizza + "</li>")
   });
 });
